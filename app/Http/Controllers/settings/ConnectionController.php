31698d821ec48a0192d2e142164a63a6c12a268a
Brijesh <brijesh.qwesys@gmail.com>
2022-01-24 20:02:41 +0530

    changes in connectioncontroller

--- app/Http/Controllers/settings/ConnectionController.php
+++ app/Http/Controllers/settings/ConnectionController.php
@@ -8,6 +8,18 @@ use Illuminate\Http\Request;
 class ConnectionController extends Controller
 {
     public function index(Request $request) {
-        echo "Hello";
+        $servername = "localhost";
+        $username = "akashs_laoffline123";
+        $password = "laoffline123";
+        $database = "akashs_laoffline";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
     }
 }
